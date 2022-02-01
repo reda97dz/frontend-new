4625313d6fa9052bb3b6b928b8a0cff84f9d55ff
Reda Baka <17656625+reda97dz@users.noreply.github.com>
2022-02-01 09:14:55 +0100

    fix: use custom drop down select instead of select tag to fix member state issue

--- src/components/Issue/Member/Member.tsx
+++ src/components/Issue/Member/Member.tsx
@@ -3,13 +3,15 @@ import { useAppDispatch, useAppSelector } from "app/hooks";
 import { nextStep, selectIssue, setMember, setMemberState } from "app/issueSlice";
 import { fetchMembers } from 'app/membersSlice'
 import { fetchMemberById, selectMember } from "app/memberSlice";
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 
 export const Member: FC = () => {
   const dispatch = useAppDispatch();
   const members = useAppSelector(selectMembers)
   const member = useAppSelector(selectMember)
   const issue = useAppSelector(selectIssue);
+
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     getMembers()
@@ -31,15 +33,7 @@ export const Member: FC = () => {
   // const handleClick = () => dispatch(fetchMembers())
   return (
     <div>
-      <select name="Member" id="member" defaultValue={'DEFAULT'} onChange={handleChange}>
-        <option value="DEFAULT" disabled > -- select an option -- </option>
-        {members.map((m) => (
-          <option key={Math.random()} value={m.id}> {m.firstName} </option>
-        ))}
-      </select>
-      {/* <button type="button" onClick={handleClick}>
-        {status === 'pending' ? 'loading members' : status === 'failed' ? 'failed' : status === 'succeeded' ? "loaded members" : 'CLick'}
-      </button> */}
+
       <input type="text" value={issue.memberId} placeholder='select Member' onChange={(e) => dispatch(setMember(e.target.value))} />
       {issue.memberId.length > 0 ? (
         <button type='button' onClick={() => dispatch(nextStep())}>
@@ -48,6 +42,22 @@ export const Member: FC = () => {
       ) : (
         <>Please select a member to continue</>
       )}
+
+      <br />
+
+      <button type='button' onClick={() => setOpen(!open)}>{member.firstName || `select a member`}</button>
+      {open && (
+        <ul>
+          {members.map((m) => (
+            <li key={m.id} onClick={() => { dispatch(fetchMemberById(Number(m.id))); dispatch(setMemberState(3 - m.active.length)) }} >{m.firstName}</li>
+          ))}
+        </ul>
+      )}
+
+      {/*
+      <button type="button" onClick={handleClick}>
+        {status === 'pending' ? 'loading members' : status === 'failed' ? 'failed' : status === 'succeeded' ? "loaded members" : 'CLick'}
+      </button> */}
     </div>
   );
 };
